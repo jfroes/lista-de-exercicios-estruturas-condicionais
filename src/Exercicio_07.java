c2047578bee6c554b061c04956a61f57bc9be1c3
José Froes <josepaulo.froes@gmail.com>
2025-11-11 14:53:46 -0300

    Exercicio 7: Mostre “Bom dia”, “Boa tarde” ou “Boa noite” de acordo com a hora.

---
+++ src/Exercicio_07.java
@@ -0,0 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio_07 {
+//  07 - Mostre “Bom dia”, “Boa tarde” ou “Boa noite” de acordo com a hora.
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("HH:mm");
+
+        System.out.print("Digite um horário (formato 24h): ");
+        LocalTime time = LocalTime.parse(sc.next(), df);
+
+
+        if (time.getHour() >= 00 && time.getHour() < 05 || time.getHour() >=18) {
+            System.out.println("Boa noite");
+        }else if(time.getHour() >= 05 && time.getHour() < 12){
+            System.out.println("Bom dia");
+        }else if (time.getHour() >= 12 && time.getHour() < 18){
+            System.out.println("Boa tarde");
+        }
+    }
+}
+
